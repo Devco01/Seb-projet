eddb8fea412a0a4199a6c2ec63b29336c637118e
Devco01 <pepsiman15.1@gmail.com>
2025-03-16 22:35:12 +0100

    Correction du conflit de fusion dans src/app/api/clients/[id]/route.ts

---
+++ src/app/api/clients/id/route.ts
@@ -0,0 +1,11 @@
+﻿import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+// Configuration pour le mode standalone
+export const dynamic = 'force-dynamic';
+
+type RouteParams = {
+  params: {
+    id: string;
+  };
+};
