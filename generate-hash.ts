b2686a79190d2ffec8de1d59c5bddb5733ee6b4a
Devco01 <your-email@example.com>
2025-07-17 19:36:16 +0200

    nettoyage du code global

--- generate-hash.ts
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const password = 'votre_mot_de_passe'; // Remplacez par votre mot de passe souhaité
-
-bcrypt.hash(password, 10).then(hash => {
-  console.log('Hash généré :', hash);
-}); 
